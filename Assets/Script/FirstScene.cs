2a735da7baa074e3707087037d2ed18a56d57ca2
balvin0059 <wwwcomtw82@hotmail.com>
2018-09-06 13:29:56 +0800

    20180906
    新增主視覺介面

---
+++ Assets/Script/FirstScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScene : MonoBehaviour {
+    public GameObject loading;
+	// Use this for initialization
+	void Start () {
+        loading.GetComponent<Loading>().GotoScene("MainScene");
+	}
+}
